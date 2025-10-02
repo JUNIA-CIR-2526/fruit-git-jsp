2a5045f27b33165fad7561fe08cb68ddc30b8b87
Luca Sfiotti <sfiottiluca@gmail.com>
2025-10-02 09:51:26 +0200

    Fruit done

---
+++ src/main/java/com/jad/Fruit.java
@@ -0,0 +1,13 @@
+package com.jad;
+
+public class Fruit {
+    private final String name;
+
+    public Fruit(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
